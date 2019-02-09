12c6aab74e2411c20132a69c7bd329d57cb60f1e
Davide Laezza <truzzialrogo@gmx.com>
2019-02-09 13:51:28 +0100

    Implementing non-remote support for mercurial

--- lib/hg.sh
+++ lib/hg.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
 zygal_hg_prompt_info() {
-    true
+    local STATUS
+    STATUS="$(hg status -marduT '{status}' 2> /dev/null)"
+    [ $? -ne 0 ] && return
+
+    grep '\(M\|A\|R\)' <<<"$STATUS" &> /dev/null && local DIRTY='+'
+    grep '?' <<<"$STATUS" &> /dev/null && local UNTRACKED='%%'
+    grep '!' <<<"$STATUS" &> /dev/null && local MISSING='!'
+    [ -n "$(hg shelve -l 2> /dev/null)" ] && local SHELVES='$'
+
+    hg identify -T "{separate(' ',
+            if(activebookmark,activebookmark,branch),
+            '${DIRTY}${SHELVES}${UNTRACKED}${MISSING}')}" \
+        | xargs -i printf "$1" '{}' ' hg'
 }
