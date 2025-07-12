110c8efea59af23af6dbf68f27b9ba01d1017057
Davide Laezza <truzzialrogo@gmx.com>
2025-07-12 17:46:02 +0200

    test(prompt): add git_info unit tests

--- zygal-prompt/src/git_info.rs
+++ zygal-prompt/src/git_info.rs
@@ -18,9 +18,8 @@ pub fn make_git_info(git_status_output: &str) -> Result<String, ZygalError> {
     let unstaged_regex = Regex::new(r"^[12u] .[MTARCDU]").unwrap();
     let unstaged = any_or_empty(lines.iter(), |l| unstaged_regex.is_match(l), "*");
 
-    Ok(format!(
-        "{branch_name} {unstaged}{staged}{stash}{untracked}{remote}"
-    ))
+    let git_info = format!("{branch_name} {unstaged}{staged}{stash}{untracked}{remote}");
+    Ok(git_info.trim().to_string())
 }
 
 fn make_branch_name<'a>(
@@ -87,5 +86,184 @@ where
 
     fn at(&mut self, at: usize) -> Result<T, ZygalError> {
         self.nth(at).ok_or(ZygalError::GitOutputError)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn error_when_no_branch_info() {
+        let status_output = "
+            # stash number-not-used
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert_eq!(git_info, Err(ZygalError::GitOutputError));
+    }
+
+    #[test]
+    fn uses_branch_name_when_not_detached_head() {
+        let branch_name = "feature/honey-badgers";
+        let status_output = format!(
+            "
+            # branch.oid unused-invalid-sha
+            # branch.head {branch_name}
+            "
+        );
+        let git_info = make_git_info(status_output.trim());
+        assert_eq!(git_info.as_deref(), Ok(branch_name));
+    }
+
+    #[test]
+    fn uses_commit_sha_when_detached_head() {
+        let status_output = "
+            # branch.oid faeddf84c5077e7df0025334801d379bb94fc64f
+            # branch.head (detached)
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert_eq!(git_info.as_deref(), Ok("(faeddf8...)"));
+    }
+
+    #[test]
+    fn includes_stashes() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/spider-monkey
+            # stash number-not-used
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("$")))
+    }
+
+    #[test]
+    fn includes_untracked() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/hippopotamus
+            ? path-not-used
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("%")))
+    }
+
+    #[test]
+    fn includes_staged() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/gorgonopsia
+            1 Dwhatever
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("+")))
+    }
+
+    #[test]
+    fn includes_unstaged() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/microraptoria
+            1 WD
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("*")))
+    }
+
+    #[test]
+    fn includes_remote_on_par() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/placoderms
+            # branch.ab +0 -0
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("=")))
+    }
+
+    #[test]
+    fn includes_remote_with_outgoing() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/ichthyosauria
+            # branch.ab +7 -0
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains(">")))
+    }
+
+    #[test]
+    fn includes_remote_with_incoming() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/hyaenodonta
+            # branch.ab +0 -3
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("<")))
+    }
+
+    #[test]
+    fn includes_diverged_remote() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/ceratopsia
+            # branch.ab +9 -3
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert!(git_info.is_ok_and(|info| info.contains("<>")))
+    }
+
+    #[test]
+    fn includes_branch_staged_untracked_incoming_remote() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/monothremes
+            # branch.ab +0 -12
+            2 D.
+            1 M.
+            ? not-used-file-name
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert_eq!(git_info.as_deref(), Ok("feature/monothremes +%<"))
+    }
+
+    #[test]
+    fn includes_sha_stash_unstaged_on_par_remote() {
+        let status_output = "
+            # branch.oid dfcac0b563036735405591415163566f9f908e1e
+            # branch.head (detached)
+            # branch.ab +0 -0
+            # stash number-not-used
+            u .D
+            2 .R
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert_eq!(git_info.as_deref(), Ok("(dfcac0b...) *$="))
+    }
+
+    #[test]
+    fn includes_branch_staged_unstaged_no_remote() {
+        let status_output = "
+            # branch.oid unused-invalid-sha
+            # branch.head feature/hymenoptera
+            u .D
+            2 D.
+            1 DD
+        "
+        .trim();
+        let git_info = make_git_info(status_output);
+        assert_eq!(git_info.as_deref(), Ok("feature/hymenoptera *+"))
     }
 }
